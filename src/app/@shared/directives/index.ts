62679c431727ffaf8df5f24c82a8daa499b8d8cc
Adenilson Junior <adenilsonn.souza@gmail.com>
2023-01-02 10:05:33 -0300

    chore: create script run jest && update version project

---
+++ src/app/@shared/directives/index.ts
@@ -0,0 +1,5 @@
+export * from './currency/currency.directive';
+export * from './focus-back/focus-back.directive';
+export * from './focus-trap/focus-trap.directive';
+export * from './keyboard-manager/keyboard-managed-item.directive';
+export * from './keyboard-manager/keyboard-manager.directive';
